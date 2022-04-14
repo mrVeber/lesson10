1b244b97cac2159531064bae1688015f11fe6ba0
bobylev <bobylev@DESKTOP-BKCVISV>
2022-04-14 11:19:18 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите градусы: ");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты дуги: ");
+            int min_ = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите угловые секунды: ");
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+
+            RadianCalc radianCalc = new RadianCalc(gradus_, min_, sec_);
+
+            Console.Write($"Угол в радианах равен: {radianCalc.ToRadiant()}");
+
+        }
+    }
+}
---
+++ task1/RadianCalc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class RadianCalc
+    {
+        int gradus;
+        int min;
+        int sec; 
+       public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                    gradus = value % 360;   
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                min = value % 60;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                min += value / 60;
+                sec = value % 60;
+            }
+        }
+        public RadianCalc(int gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+        public double ToRadiant()
+        {
+            return (double)(gradus * Math.PI / 180) + (min * Math.PI / 60 * 180) + (sec * Math.PI / 180 * 3600);
+
+        }
+    }
+}
